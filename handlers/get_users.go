5a938d312aa1b6aa92681b8f7e0e82579278df0f
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-08-20 21:59:40 +0530

    feat/working-imeme-list-rendering

--- handlers/get_users.go
+++ handlers/get_users.go
@@ -1,10 +1,20 @@
 package handler
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/arnavmahajan630/checkout-htmx/models"
+	"github.com/arnavmahajan630/checkout-htmx/views/user"
+)
 
 func GetUsers(w http.ResponseWriter, r * http.Request) error{
-	htmlContent := "<h2>Content Loaded Successfully!</h2>"
-	w.Header().Set("Content-Type", "text/html")
-	_ , err := w.Write([]byte(htmlContent))
-	return err
+	slog.Info("Serving in memory user list")
+
+	myUsers := []models.User {
+		{Id: 1, Name: "Arnav", Email: "example@gmail.com"},
+		{Id: 2, Name: "Jhon", Email: "example@gmail.com"},
+		{Id: 3, Name: "Random", Email: "example@gmail.com"},
+	}
+	return Render(w, r, user.List(myUsers))
 }
